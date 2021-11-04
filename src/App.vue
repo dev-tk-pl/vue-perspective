53189e159c58259231d75c8242d12ace5f384736
dev-tk-pl <dev_tk@int.pl>
2021-11-04 13:00:46 +0100

    improving the operation of the perspective

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="img-wrapper" @mousemove="getCursorPosition">
     <img
-      :style="`transform: perspective(300px) rotateX(${x}deg) rotateY(${y}deg) `"
+      :style="`transform: perspective(300px) rotateX(${x}deg) rotateY(${y}deg)`"
       alt="me"
       src="./assets/me.png"
     />
@@ -27,9 +27,13 @@ export default {
       // pageX/Y gives the coordinates relative to the <html> element in CSS pixels.
       console.log((event.pageX / window.innerWidth).toFixed(2));
       console.log((event.pageY / window.innerHeight).toFixed(2));
-
-      this.y = (event.pageX / window.innerWidth).toFixed(2) * 20;
-      this.x = (event.pageY / window.innerHeight).toFixed(2) * 20;
+     /* eslint-disable */
+      this.y =
+        ((event.pageX - window.innerWidth / 2) 
+        / window.innerWidth).toFixed(2) * 34;
+      this.x =
+        ((event.pageY - window.innerHeight / 2) 
+        / window.innerHeight).toFixed(2) * -34;
 
       // screenX/Y gives the coordinates relative to the screen in device pixels.
       // console.log("screenX : " + event.screenX);
@@ -53,14 +57,16 @@ body {
   color: #2c3e50;
 }
 .img-wrapper {
-  width: 100vw;
-  height: 100vh;
+  max-width: 100vw;
+  min-height: 100vh;
   background: url("./assets/bg_home.jpg");
+  display: flex;
+  justify-content: center; /* align horizontal */
+  align-items: center;
 }
 img {
-  margin-top: 100px;
-  width: 500px;
-  height: auto;
+  width: auto;
+  height: 70vh;
   /* transform: perspective(300px) rotateX(20deg); */
 }
 </style>
